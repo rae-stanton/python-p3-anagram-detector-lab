38dd115ee248a765bff1ae81af556b7a9ed26570
Rae Stanton <raestanton93@gmail.com>
2023-09-12 09:59:02 -0700

    anagram fin

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,6 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, candidates):
+        return [candidate for candidate in candidates if sorted(candidate.lower()) == sorted(self.word.lower()) and candidate.lower() != self.word.lower()]
